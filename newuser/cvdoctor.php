7b3de0d2adbfcf1699dfe26db9da079307af7799
MANAS VERMA <manasverma200@gmail.com>
2024-07-18 20:17:23 +0530

    Add files via upload

---
+++ newuser/cvdoctor.php
@@ -0,0 +1,3 @@
+<?php
+include '../php/cvdoctor.php';
+?>
---
+++ php/cvdoctor.php
@@ -0,0 +1,45 @@
+<?php
+if (($_FILES['file']['name']!="")){
+    // Where the file is going to be stored
+        $target_dir = "../cv/";
+        $file = $_FILES['file']['name'];
+        $path = pathinfo($file);
+        $filename = $path['filename'];
+        $ext = $path['extension'];
+        $temp_name = $_FILES['file']['tmp_name'];
+        $path_filename_ext = $target_dir.$filename.".".$ext;
+}
+    $fullname = $_POST['fullname'];
+    $email = $_POST['email'];
+    $address = $_POST['address'];
+    $city = $_POST['city'];
+    $zip = $_POST['zip'];
+    $tel = $_POST['tel'];
+    $exp = $_POST['exp'];
+    $qua = $_POST['qua'];
+    $spec = $_POST['spec'];
+    $file = $filename.".".$ext;
+
+    include'../php/dbconn.php';
+if (mysqli_connect_error()){
+    die('Connect Error('.mysqli_connect_error().')');
+}
+else{
+    $sql = "INSERT INTO jdoctor (fullname,email,address,city,zip,exp,qua,spec,tel,cv)
+    values('$fullname','$email','$address','$city','$zip','$exp','$qua','$spec','$tel','$file')";
+    if ($con->query($sql)){
+        move_uploaded_file($temp_name,$path_filename_ext);
+            include 'header.php';
+            include '../php/fileuploaded.php';
+            
+      exit();
+    }
+    else{
+        include 'header.php';
+            include '../php/filenotuploaded.php';
+    }
+    
+    $con->close();
+}
+
+?>
